02bc378883bfcf83b389944f86be71d591174915
gpx <1457495424@qq.com>
2018-08-12 18:36:43 +0800

    add some content, processed the input file path does not exist

--- longest_content.sh
+++ longest_content.sh
@@ -4,6 +4,7 @@
 max_length=0
 max_file=""
 word=""
+flag=1
 
 function find_maxone() {
     #words=`cat $1 | tr -s -c "a-zA-Z" "\n"`
@@ -15,7 +16,7 @@ function find_maxone() {
             max_length=$temp_length
             max_file=$1
             word=$a
-            echo $max_length
+            #echo $max_length
         fi
     done
 }
@@ -24,6 +25,7 @@ function listFilrs() {
     maxsize=1024
     #1st param, the dir name
     for file in `ls $1`; do
+        flag=0
         if [[ -d "$1/$file" ]]; then
             #echo "$file"
             listFilrs "$1/$file"
@@ -37,8 +39,14 @@ function listFilrs() {
     done
 }
 
-listFilrs "/home/gpx"
-echo "最长的字符串长度:"$max_length
-echo "最长的字符串所在的文件位置:"$max_file
-echo "最长的字符串内容:"$word
+
+listFilrs $1
+
+if [[ flag -eq 0 ]]; then
+    echo "最长的字符串长度:"$max_length
+    echo "最长的字符串所在的文件位置:"$max_file
+    echo "最长的字符串内容:"$word
+else
+    echo "该文件或目录不存在"
+fi
 
