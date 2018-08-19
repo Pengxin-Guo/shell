11ef688303b05160913f1b7adf27353d19eea0ac
gpx <1457495424@qq.com>
2018-08-19 17:07:39 +0800

    modify mylog.sh

--- mylog.sh
+++ mylog.sh
@@ -2,7 +2,7 @@
 
 echo "# 记录用户登录信息" >> /home/gpx/.bashrc  
 cat >> /home/gpx/.bashrc << EOF
-TIME_NOW=\`date -u +"%Y-%m-%d %H:%M"\`
+TIME_NOW=\`date +"%Y-%m-%d %H:%M"\`
 USER_NOW=\`whoami\`
 LOGIN_TIME=\`date +%s\`
 echo "\$TIME_NOW \$USER_NOW LOGIN" >> /home/gpx/trash/mylog.log 
@@ -10,7 +10,7 @@ EOF
 
 echo "# 记录用户登出信息" >> /home/gpx/.bash_logout 
 cat >> /home/gpx/.bash_logout << EOF
-TIME_NOW=\`date -u +"%Y-%m-%d %H:%M"\`
+TIME_NOW=\`date +"%Y-%m-%d %H:%M"\`
 USER_NOW=\`whoami\`
 LOGOUT_TIME=\`date +%s\`
 echo "\$TIME_NOW \$USER_NOW LOGOUT last \$[(\$LOGIN_TIME-\$LOGOUT_TIME)/60] minute(s)" >> /home/gpx/trash/mylog.log 
