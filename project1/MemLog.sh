f2d6762693589142813b51dcf39e6813db396917
gpx <1457495424@qq.com>
2018-08-26 10:32:55 +0800

    improve MemLog.sh by using array

--- project1/MemLog.sh
+++ project1/MemLog.sh
@@ -7,8 +7,9 @@ fi
 
 last=$1                                                                       #last代表上一个动态平均值,由程序传入这个参数
 date=`date +"%Y-%m-%d__%T"`                                                   #date代表当前时间
-total=`free -m | awk ' NR == 2 {printf $2}'`                                  #total代表总内存
-used=`free -m | awk ' NR == 2 {print $3}'`                                    #used代表当前占用内存
+arr_mes=(`free -m | awk ' NR == 2 {print $2 " " $3} '`)                       #arr_mes数组存放一些内存信息
+total=${arr_mes[0]}                                                           #total代表总内存
+used=${arr_mes[1]}                                                            #used代表当前占用内存
 remaining=$[$total-$used]                                                     #remaining代表内存剩余量
 current=`echo "scale=1;${used}*100/${total}" | bc`                            #current代表当前内存占用率
 dynamic_average=`echo "scale=1;0.3*${last}+0.7*${current}" | bc`              #dynamic_average代表占用百分比动态平均值
