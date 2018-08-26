52f01cd043237e2405d3661b6b7bc77a88755b2e
gpx <1457495424@qq.com>
2018-08-26 20:51:41 +0800

    add some notes

--- DSMS_project/DiskLog.sh
+++ DSMS_project/DiskLog.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 date=`date +"%Y-%m-%d__%T"`                  #date代表当前时间
 eval `df -m | grep "^/dev" | awk '{printf("arr_total["NR"]=%d;arr_used["NR"]=%d;arr_percent["NR"]=%s;arr_path["NR"]=%s\n",$2,$3,$5,$6)}'`
-total=0                                      #total代表磁盘总量
-used=0                                       #used代表磁盘占用量
+total=0                                      #total代表磁盘总量,单位是M
+used=0                                       #used代表磁盘占用量,单位是M
 
 for i in `seq 1 ${#arr_total[@]}`;do
     total=$[$total+${arr_total[$i]}]
@@ -10,7 +10,7 @@ for i in `seq 1 ${#arr_total[@]}`;do
 done
 
 percent=$[$used*100/$total]                  #percent代表磁盘占用比例
-remaining=$[$total-$used]                    #remaining代表磁盘剩余量
+remaining=$[$total-$used]                    #remaining代表磁盘剩余量,单位是M
 
 echo $date 0 disk $total $remaining ${percent}%
 for i in `seq 1 ${#arr_total[@]}`;do
