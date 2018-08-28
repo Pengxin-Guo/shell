0089fe40615fcf9a3abc1401e1a07489d1e4636d
gpx <1457495424@qq.com>
2018-08-28 12:52:35 +0800

    modify CPULog.sh

--- DSMS_project/CPULog.sh
+++ DSMS_project/CPULog.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 date=`date +"%Y-%d-%m__%T"`    #date代表当前时间
+load=`cat /proc/loadavg | cut -d " " -f 1-3`  #load代表3个负载负载
+#cat /proc/loadavg 中
+#前三个值分别代表系统5分钟、10分钟、15分钟前的平均负载
+#第四个值的分子是正在运行的进程数，分母为总进程数
+#第五个值是最近运行的进程id
