01fbe3cabc5888c118598a3cc13a28efb19afd0d
gpx <1457495424@qq.com>
2018-08-29 16:21:26 +0800

    add some notes in Users.sh

--- DSMS_project/Users.sh
+++ DSMS_project/Users.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#用户信息统计
+#该脚本为统计一些用户信息
 #输出当前时间、用户总数(非系统用户) 近期活跃用户(3个) 具有ROOT权限用户 当前在线用户_登录IP_TTY
+
 date=`date +"%Y-%m-%d__%T"`                                                                                                             #date代表当前时间
 num_users=`cat /etc/passwd | grep "bash" | wc -l`                                                                                       #num_users代表用户总数
 active_users=`last | head -n -2 | cut -d " " -f 1 | sort | uniq -c | sort -n -r | head -n 3 | awk '{print $2}' | xargs | tr " " ","`    #active_users代表近期最活跃的3个用户
