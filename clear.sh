894bce4c28d5d8d5e2f649036ba96f8f2fef3eca
gpx333 <37725726+gpx333@users.noreply.github.com>
2018-09-02 19:10:33 +0800

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -2,4 +2,4 @@
 #定期删除/home/gpx/trash中的文件
 
 cd /home/gpx/trash                     #首先进入该目录
-find -mtime +7 -exec rm -rf {} \;    #然后删除该目录下7天前的所有文件,包括其中的子目录
+find -mtime +7 -exec rm -rf {} \;      #然后删除该目录下7天前的所有文件,包括其中的子目录
