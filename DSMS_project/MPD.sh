d804c3724c50d48cc544c5309caa3ce5997912e8
gpx <1457495424@qq.com>
2018-09-12 20:13:41 +0800

    modify MPD.sh and use ps to come true it

--- DSMS_project/MPD.sh
+++ DSMS_project/MPD.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 #检测恶意进程，在5s内如果某一进程的CPU或者内存占用超过50%，视为恶意进程
 
-data=`top -c | head -n 10 | awk 'NR == 8 {print}'`    #data代表top显示出的第一个进程信息
-pid=`echo $date | cut -d " " -f 2`                    #pid代表这个进程的ID
-cpu=`echo $date | cut -d " " -f 10`                   #cpu代表这个进程的cpu占用
-
-echo $cou $mem
-if [ `echo "$cpu > 50" | bc` -eq 1 ];then
-    sleep 5
-    data=`top -p $pid -n 1 | awk 'NR == 8 {print}'`
-    cpu=`echo $date | cut -d " " -f 10`                  
-    mem=`echo $date | cut -d " " -f 10`
-    if [ `echo "$cpu > 50" | bc` -eq 1 ];then
-        date=`date +"%Y-%m-%d__%T"`
-        pname=`echo $data | cut -d " " -f 13`
-        user=`echo $data | cut -d " " -f 3`
-        echo $date $pname $pid $user ${CPU}% ${mem}%
-    fi
-fi
-
+ps -aux | tr -s " " | sort -t" " -k 3 -r | head -n 10 | tail -n 9 > Process.txt
+for i in `seq 1 9`;do
+    data[$i]=$(awk 'NR == $i {print}' ./Process.txt)
+done
+for i in `seq 1 9`;do
+    echo ${data[$i]};
+done
