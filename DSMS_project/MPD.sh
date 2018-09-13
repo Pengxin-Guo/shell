dc05fc121569df1c3e9cc5b9cd3f78445d664e63
gpx <1457495424@qq.com>
2018-09-13 16:25:57 +0800

    add MPD.sh

--- DSMS_project/MPD.sh
+++ DSMS_project/MPD.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #检测恶意进程，在5s内如果某一进程的CPU或者内存占用超过50%，视为恶意进程
 
-ps -aux | tr -s " " | sort -t" " -k 3 -r | head -n 10 | tail -n 9 > Process.txt
-for i in `seq 1 9`;do
-    data[$i]=$(awk 'NR == $i {print}' ./Process.txt)
-done
-for i in `seq 1 9`;do
-    echo ${data[$i]};
-done
+pid=`ps -aux | awk '{if($3>50.0 || $4>50.0) print $2}'`    #pid代表第一次检测的时候，CPU超过50%的进程id
+
+if [[ $pid != "" ]];then
+    sleep 5;
+    time=`date +%Y-%m-%d__%T`                              #time代表当前时间
+    for i in $pid;do 
+        ps -p $i -u | tail -n 1 | awk -v time=$time '{if($3>50.0 || $4>50.0) {printf("%s\t%-50s\t%s\t%s\t%s%%\t%s%%\n", time, $11, $2, $1, $3, $4)}}'
+    done
+fi
